e17f4262b76106a7d4ce5bd7062386d51f97f8a4
Mikechavo <mchavez76@yahoo.com>
2024-04-10 19:11:18 -0500

    added more projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,6 +4,7 @@ import JJK from '../assets/JJK.png'
 import PcPart from '../assets/pcPart.png'
 import PcTab from '../assets/tabs.png'
 import FitFolio from '../assets/fitFolio.png'
+import Flare from '../assets/flare.png'
 import Button from './Button'
 
 
@@ -53,7 +54,7 @@ export default function Projects() {
     <div className="grid grid-cols-1 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
      <img
       src={PalH}
-      alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+      alt=""
       className="rounded-lg bg-gray-100"
      />
 
@@ -98,7 +99,7 @@ export default function Projects() {
     <div className="grid grid-cols-1 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
      <img
       src={PintPal}
-      alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+      alt=""
       className="rounded-lg bg-gray-100"
      />
 
@@ -136,7 +137,7 @@ export default function Projects() {
     <div className="grid grid-cols-1 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
      <img
       src={JJK}
-      alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+      alt=""
       className="rounded-lg bg-gray-100"
      />
 
@@ -167,12 +168,12 @@ export default function Projects() {
     <div className="grid grid-cols-1 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
      <img
       src={PcPart}
-      alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+      alt=""
       className="rounded-lg bg-gray-100"
      />
      <img
       src={PcTab}
-      alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+      alt=""
       className="rounded-lg bg-gray-100"
      />
 
@@ -210,32 +211,63 @@ export default function Projects() {
     <div className="grid grid-cols-1 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
      <img
       src={FitFolio}
-      alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+      alt=""
       className="rounded-lg bg-gray-100"
      />
     </div>
    </div>
 
-      {/* FitFolio */}
+      {/* Project Flare: */}
       <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
     <div>
-     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">FitFolio</h2>
+     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Project Flare:</h2>
+     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Automated Candidate Management Tool at Uber</h2>
      <p className="mt-4 text-gray-500">
-     A Fullstack application using React, Javascript, Reactstrap, PostgreSQL and Ruby on Rails that allows you to craft your perfect workout with Fitfolio! Tailor routines to your goals, choose from a variety of exercises, and track progress effortlessly. User-friendly, flexible, and designed for your fitness journey. Stay tuned in for updates as I refine and enhance this fitness journey companion!
+     I designed, developed, and launched Project Flare at Uber, a powerful automation tool utilizing Google Apps Script, Gsheets, Gmail, and integrated it seamlessly into an internal dashboard. My role involved conceptualizing the solution, overseeing the development process, and successfully launching the tool. Project Flare efficiently identified stale candidates in the recruitment pipeline and automated personalized emails to prompt Talent Acquisition (TA) team members for data hygiene reviews. This initiative significantly enhanced the efficiency and organization of Uber's talent acquisition processes.
      </p>
+     <p className="mt-4 text-gray-500">
+     <strong><u>Key Contributions:</u></strong></p>
+     <p className="mt-4 text-gray-500">
+     <u>End-to-End Ownership:</u> Led the entire lifecycle of Project Flare, from conceptualization and design to development and successful deployment.
+     </p>
+     <p className="mt-4 text-gray-500">
+     <u>Stale Candidate Identification:</u> Systematically designed the process for scrubbing candidate ownership data to accurately identify stale candidates.
+     </p>
+     <p className="mt-4 text-gray-500">
+     <u>Automated Email Generation:</u> Orchestrated the automation of personalized emails sent to TA members, including critical candidate details and requisition information.
+     </p>
+     <p className="mt-4 text-gray-500">
+     <u>Internal Dashboard Integration: </u>  Ensured seamless integration with an internal dashboard for real-time visibility into candidate hygiene initiatives.
+     </p>
+
+     <p className="mt-4 text-gray-500">
+     <strong><u>Impact:</u></strong></p>
+     <p className="mt-4 text-gray-500">
+     Project Flare stands as a testament to my ability to take ownership of complex projects, design effective automation solutions, and drive positive outcomes. The tool significantly streamlined Uber's recruitment pipeline, showcasing my proficiency in technology-driven process improvements.
+     </p>
+
+     <p className="mt-4 text-gray-500">
+     <strong><u>Technologies Used:</u></strong></p>
+     <p className="mt-4 text-gray-500">
+     Google Apps Script(HTML, CSS, Javascript), SQL, GSheets, Gmail, Internal Dashboards
+     </p>
+     <p className="mt-4 text-gray-500">
+     iCIMS, Beamery, LinkedIn Recruiter
+     </p>
+
 
      <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
 
       <div className="border-t border-gray-200 pt-4">
-       <Button><a href="https://github.com/Mikechavo/workout_frontend">
-       Frontend Source Code
+       <Button><a href="https://github.com/Mikechavo/Project-Beacon-Flare-GAS/blob/main/flareGetEmail.js">
+      Source Code
        </a>
        </Button>
        {/* <dd className="mt-2 text-sm text-gray-500"> Python, Django framework(maybe put an image here)</dd> */}
       </div>
       <div className="border-t border-gray-200 pt-4">
-       <Button><a href="https://github.com/Mikechavo/workout_backend">
-       Backend Source Code
+       <Button><a href="https://docs.google.com/spreadsheets/d/1HIRP86Fj51r3kcNx-BltYzuDliUY_pO4gMQIVr3w6K4/edit#gid=913754463">
+       Demo
        </a>
        </Button>
        {/* <dd className="mt-2 text-sm text-gray-500"> Python, Django framework(maybe put an image here)</dd> */}
@@ -245,12 +277,55 @@ export default function Projects() {
     </div>
     <div className="grid grid-cols-1 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
      <img
-      src={FitFolio}
-      alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
+      src={Flare}
+      alt=""
       className="rounded-lg bg-gray-100"
      />
     </div>
    </div>
+
+
+
+
+{/* Tic-Tac-Toe */}
+<div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+    <div>
+     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Tic-Tac-Toe</h2>
+     <p className="mt-4 text-gray-500">
+     This is a classic Tic-Tac-Toe game designed for two players to enjoy on the same screen. The game offers a simple yet engaging experience, with players taking turns to compete in this age-old battle of Xs and Os. It's a perfect way to have fun and test your strategic skills against a friend or family member without the need for any installations or downloads.
+     </p>
+
+     <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+
+      <div className="border-t border-gray-200 pt-4">
+       <Button><a href="https://github.com/Mikechavo/my_fav_anime_react">
+        Source Code
+       </a>
+       </Button>
+       {/* <dd className="mt-2 text-sm text-gray-500"> Python, Django framework(maybe put an image here)</dd> */}
+      </div>
+      <div className="border-t border-gray-200 pt-4">
+      <Button><a href="https://my-favorite-anime.netlify.app/">
+        Live Site
+       </a>
+       </Button>
+       {/* <dd className="mt-2 text-sm text-gray-500"> Python, Django framework(maybe put an image here)</dd> */}
+      </div>
+
+     </dl>
+    </div>
+    <div className="grid grid-cols-1 grid-rows-1 gap-4 sm:gap-6 lg:gap-8">
+     <img
+      src={JJK}
+      alt=""
+      className="rounded-lg bg-gray-100"
+     />
+
+    </div>
+   </div>
+
+
+
 
 
   </div>

8b705efb3fc3ba62d231472fd8758c4eae47b2b7
Mikechavo <mchavez76@yahoo.com>
2024-04-16 16:38:21 -0500

    changed resume page

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,9 +1,14 @@
+import resumeImage from '../assets/resume_mike.png'; 
+
 const Resume = () => {
   return (
     <div className="container mx-auto mt-6">
-      <iframe src="https://docs.google.com/document/d/1SkrVFIcJuQcVxa0Asvo_5mvJ8VVSAG4yVvv79o4cBJw/edit?usp=sharing" width="100%" height="800px"></iframe>
+      <a href="https://docs.google.com/document/d/1SkrVFIcJuQcVxa0Asvo_5mvJ8VVSAG4yVvv79o4cBJw/edit#heading=h.gjdgxs" target="_blank" rel="noopener noreferrer">
+        <img src={resumeImage} alt="Resume" style={{ width: '100%', height: 'auto' }} /> {/* Correct usage of src attribute */}
+      </a>
     </div>
   );
 };
 
 export default Resume;
+

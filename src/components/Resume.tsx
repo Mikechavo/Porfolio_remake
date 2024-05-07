0f2bf3d29af133ad1525a29130372b5655876cb0
Mikechavo <mchavez76@yahoo.com>
2024-05-07 16:39:26 -0500

    changes to resume page

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,11 +1,7 @@
-import resumeImage from '../assets/resume_mike.png'; 
-
 const Resume = () => {
   return (
     <div className="container mx-auto mt-6">
-      <a href="https://docs.google.com/document/d/1SkrVFIcJuQcVxa0Asvo_5mvJ8VVSAG4yVvv79o4cBJw/edit#heading=h.gjdgxs" target="_blank" rel="noopener noreferrer">
-        <img src={resumeImage} alt="Resume" style={{ width: '100%', height: 'auto' }} /> {/* Correct usage of src attribute */}
-      </a>
+      <embed src="/resume/MichaelChavez_Resume_April_2024.pdf" type="application/pdf" width="100%" height="800px" />
     </div>
   );
 };
